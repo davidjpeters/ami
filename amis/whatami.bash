fdde13e91736cf4cd714c551dd968e581a636dc4
David Peters <davidpeters019@gmail.com>
2026-08-25 13:36:15 -0600

    Add graphics info to whatami script

--- amis/whatami.bash
+++ amis/whatami.bash
@@ -42,10 +42,41 @@ if [ -z "$uptime" ]; then
     uptime="unknown"
 fi
 
+display=$(system_profiler SPDisplaysDataType 2>/dev/null | grep "Display Type" | awk -F': ' '{print $2}')
+if [ -z "$display" ]; then
+    display="unknown"
+fi
+
+resolution=$(system_profiler SPDisplaysDataType 2>/dev/null | grep "Resolution:" | awk -F': ' '{print $2}')
+if [ -z "$resolution" ]; then
+    resolution="unknown"
+fi
+
+graphics=$(system_profiler SPDisplaysDataType 2>/dev/null | grep "Chipset Model" | awk -F': ' '{print $2}')
+if [ -z "$graphics" ]; then
+    graphics="unknown"
+fi
+
+graphics_type=$(system_profiler SPDisplaysDataType 2>/dev/null | grep -m 1 "Type:" | awk -F': ' '{print $2}')
+if [ -z "$graphics_type" ]; then
+    graphics_type="unknown"
+fi
+
+gpu_cores=$(system_profiler SPDisplaysDataType 2>/dev/null | grep "Cores" | awk -F': ' '{print $2}')
+if [ -z "$gpu_cores" ]; then
+    gpu_cores="unknown"
+fi
+
 echo "OS: "$os""
 echo "Kernel: "$kernel""
 echo "Model Name: "$model""
 echo "Uptime: "$uptime""
 echo "Chip: "$chip""
 echo "Cores: "$cores""
+echo "Graphics: "$graphics""
+echo "Type: "$graphics_type""
+echo "Graphic Cores: "$gpu_cores""
+
 echo "Memory: "$memory""
+echo "Display: "$display""
+echo "Resolution: "$resolution""
